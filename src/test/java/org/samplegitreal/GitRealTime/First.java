cb0c15518ec7b00de850ba7f51072bb866265be0
somu190296 <somasundaramooosivam@gmail.com>
2021-05-07 16:24:16 +0530

    manager push

---
+++ src/test/java/org/samplegitreal/GitRealTime/First.java
@@ -0,0 +1,19 @@
+package org.samplegitreal.GitRealTime;
+
+public class First {
+	
+	private void method1() {
+		System.out.println("mtd1");
+
+	}
+	private void method2() {
+		System.out.println("mtd2");
+
+	}
+	private void method3() {
+		System.out.println("mtd3");
+
+	}
+	
+
+}
