da24804ef55831038cfe28c0c546d50397271a33
Mugil96 <mugilk26@gmail.com>
2021-05-07 17:27:37 +0530

    mugil push rework

--- src/test/java/org/samplegitreal/GitRealTime/First.java
+++ src/test/java/org/samplegitreal/GitRealTime/First.java
@@ -24,4 +24,15 @@ public class First {
     private void method6() {
        System.out.println("mtd6");
 	}
+    private void method10() {
+		System.out.println("mtd10");	
+		}
+    private void method11() {
+		System.out.println("mtd11");
+
+	}
+    private void method12() {
+		System.out.println("mtd12");
+
+	}
 }
